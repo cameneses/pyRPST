5575ed963f8c1f4bc61b04e5743e853127a7eb3b
Cesar Meneses <cnmeneses@uc.cl>
2020-03-10 13:13:26 -0300

    add empty graph and e2o in tcskeleton

--- tctree/tcskeleton.py
+++ tctree/tcskeleton.py
@@ -4,12 +4,16 @@ class TCSkeleton(DirectedGraph):
 
     def __init__(self, graph = None, e2o = None):
         super().__init__()
-        self.graph = graph
+        if graph:
+            self.graph = graph
+            self.e2o = e2o
+            self.map_edges()
+            self.copy_graph()
+        else: 
+            self.graph = []
+            self.e2o = []
         self.virtual_edges = []
-        self.e2o = e2o
-        self.map_edges()
-        self.copy_graph()
-
+        
     def map_edges(self):
         for e in self.graph.get_edges():
             se = self.add_edge(e.get_source(), e.get_target(), None)

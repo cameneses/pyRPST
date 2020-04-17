dc488964d11134048833a1bf9e1e9accf2f7222c
JacielUAT <54591955+JacielUAT@users.noreply.github.com>
2020-04-16 20:32:40 -0500

    Set new fuctions set_name and get_name

--- tctree/tctnode.py
+++ tctree/tctnode.py
@@ -11,10 +11,15 @@ class TCTreeNode:
     def __init__(self):
         self.type = TCTreeNode.UNDEFINED
         self.skeleton = TCSkeleton()
+        self.name =""
     def getType(self):
         return self.type
     def get_skeleton(self):
         return self.skeleton
+    def get_name(self):
+        return self.name
+    def set_name(self,name):
+        self.name =name
     def __str__(self):
         edges = ''
         for e in self.skeleton.get_edges():
